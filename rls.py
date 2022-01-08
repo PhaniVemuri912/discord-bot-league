d3d20d09cb3b778d7a301d1dabd273fb325d17c0
Phani Vemuri <ph.vemuri@gmail.com>
2022-01-08 12:36:50 +0000

    include discord

--- rls.py
+++ rls.py
@@ -1,8 +1,40 @@
 import random
+import os
+import discord
 
-lanes = ['Top','ADC','Support','Jungle','Mid']
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the auto fill league!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == 'roll-the-dice':
+        lanes_returned = rls()
+        await message.channel.send(lanes_returned)
 
 def rls():
+    lanes = ['Top','ADC','Support','Jungle','Mid']
     i = 5
     selected_lanes = []
     while i > 0:
@@ -15,4 +47,6 @@ def rls():
         i = i-1
     return selected_lanes
 
-print(rls())
+client.run(TOKEN)
+
+
