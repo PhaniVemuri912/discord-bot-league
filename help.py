6ed7380d1abbaa0b81e8bd5dc65e46c68d9d5f8f
reuben <reubenteh2005@gmail.com>
2022-01-11 13:09:50 +0000

    installed help fuction

---
+++ help.py
@@ -0,0 +1,7 @@
+import os
+import discord
+while True:
+   text = input()
+   if text == "*help":
+      print("*s 'lane' will randomly select a champion from a lane of your choice")
+      print("*roll followed by any number of names will generate a random lane for each person. Remaining lanes will be assigned to Random.") 
