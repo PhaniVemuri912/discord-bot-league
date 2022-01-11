bd39fc561b9c15bd780420266c1e89f3aa2e36ac
Phani Vemuri <ph.vemuri@gmail.com>
2022-01-11 12:40:25 +0000

    champ selection

--- rls.py
+++ rls.py
@@ -29,6 +29,12 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
+    if '*select' in message.content.lower():
+        message_string = message.content
+        names = message_string.split()
+        names.remove('*select')
+        hero_selected = rcs(names[0])
+        await message.channel.send(hero_selected)     
     if '*roll' in message.content.lower():
         message_string = message.content
         names = message_string.split()
@@ -59,6 +65,16 @@ def rls(names):
         i = i+1
         j = j-1
     return name_and_lanes
+
+
+def rcs(name):
+    top = ['Aatrox', 'Akali', 'Akshan', 'Camille', 'Cassiopeia', 'ChoGath', 'Darius', 'Dr. Mundo', 'Fiora', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Gwen', 'Heimerdinger', 'Illaoi', 'Irelia', 'Jax', 'Jayce', 'Kayle', 'Kennen', 'Kled', 'Malphite', 'Mordekaiser', 'Nasus', 'Ornn', 'Poppy', 'Quinn', 'Renekton', 'Riven', 'Ryze', 'Sett', 'Shaco', 'Shen', 'Singed', 'Sion', 'Sylas', 'Tahm Kench', 'Teemo', 'Trundle', 'Tryndamere', 'Urgot', 'Viktor', 'Vladimir', 'Warwick', 'Wukong', 'Yasuo', 'Yone', 'Yorick']  
+    jungle = ["Amumu", "Brand", "Chogath", "Diana", "Dr. Mundo", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Gragas", "Graves", "Hecarim", "Ivern", "JarvenIV", "Jax", "Karthus", "Kayn", "Kha'Zix", "Kindred", "Kled", "Lee Sin", "Lillia", "Master Yi", "Nidalee", "Nocturne", "NUNU", "Olaf", "Ornn", "Poppy", "Rammu", "Reksai", "Rengar", "Sejuani", "Sett", "Shaco", "Shyvana", "Skarner", "Sylas", "Taliya", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", "Xin Zhao", "Zac"]
+    mid = ["Aatrox", "Ahri", "Akali", "Akshan", "Anivia", "Annie", "Aurelion Sol", "Azir", "Brand", "Camile", "Cassiopeia", "Chogath", "Corki", "Ekko", "Fizz", "Galio", "Garen", "Heimerdinger", "Irelia", "Ivern", "Jayce", "Kassadin", "Katarina", "Leblanc", "Lillia", "Lissandra", "Lucian", "Lux", "Malphite", "Malzahar", "Neeko", "Nocturne", "Nunu", "Orianna", "Patheon", "Pyke", "Qiyana", "Renekton", "Rumble", "Ryze", "Swain", "Sylas", "Syndra", "Talon", "Twisted Fate", "Viegar", "VelKoz", "Viktor", "Vladimir", "Xerath", "Yasuo", "Zed", "Ziggs", "Zilean", "Zoe", "Trydamere"]
+    adc = ["Akshan", "Aphelios", "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Jhin", "Jinx", "Kaisa", "Kalista", "KogMaw", "Lucian", "Miss Fortune", "Samira", "Senna", "Quinn", "Sivir", "Veigar", "Tristana", "Twitch", "Varus", "Vayne", "Xayah", "Yasuo", "Yone"]
+    support = ["Alistar", "Bard", "Blitzcrank", "Brand", "Braum", "Galio", "Janna", "Karma", "Karthus", "Leona", "Lulu", "Lux", "Maokai", "Miss Fortune", "Morgana", "Nami", "Nautilus", "Pantheon", "Pyke", "Rakan", "Rell", "Senna", "Seraphine", "Shaco", "Sona", "Soraka", "Swain", "Taric", "Teemo", "Thresh", "Twitch", "VelKoz", "Vex", "Xerath", "Yuumi", "Zilean", "Zyra"]
+    randselection = random.randint(0,len(locals()[name])-1)
+    return locals()[name][randselection]
 
 client.run(TOKEN)
 
